c682871696d0698857a93b9bb7f79396f5157cde
leed21542 <leed21542@naver.com>
2021-02-17 14:08:16 +0900

    MockMvc테스트 완료

---
+++ src/test/java/com/example/demo/controller/HelloWorldControllerTest.java
@@ -0,0 +1,43 @@
+package com.example.demo.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class HelloWorldControllerTest {
+
+    @Autowired
+    private HelloWorldController helloWorldController;
+
+    private MockMvc mockMvc;
+
+    @Test
+    void helloWorld(){
+//        System.out.println("test"); //sout enter
+        System.out.println(helloWorldController.helloworld());
+
+        // helloWorldController가 "helloWorld"를 잘 반환하는지 검증하는 부분
+        assertThat(helloWorldController.helloworld()).isEqualTo("HelloWorld");
+    }
+
+    @Test
+    void mockMvcTest() throws Exception {
+        mockMvc = MockMvcBuilders.standaloneSetup(helloWorldController).build();
+
+        mockMvc.perform(
+                MockMvcRequestBuilders.get("/api/helloWorld")
+        ).andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isOk())
+        .andExpect(MockMvcResultMatchers.content().string("HelloWorld"));
+    }
+}
